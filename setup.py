56b22535e23c062f6b286575a4ced286df641309
Christine Kim <chkim@uber.com>
2015-09-26 23:16:51 -0700

    Changed README.md to .rst format for PyPi

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from __future__ import unicode_literals
 from setuptools import find_packages
 from setuptools import setup
 
-with open('README.md') as f:
+with open('README.rst') as f:
     readme = f.read()
 
 setup(
